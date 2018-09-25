4fa55ad9095ea27a266385a3c972372db7320372
Sebastian Riedel <git@basxto.de>
2018-09-26 01:44:25 +0200

    Add character templates of original base assets
    http://lpc.opengameart.org/static/lpc-style-guide/assets.html

---
+++ spritesheets/body/female/female_slash.tsx
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="normal" tilewidth="64" tileheight="64" tilecount="24" columns="6">
+ <properties>
+  <property name="author" value="redshrike"/>
+  <property name="category" value="body"/>
+  <property name="filter_sex" type="int" value="2"/>
+  <property name="layer" type="int" value="0"/>
+  <property name="license" value="CC BY-SA 3.0;GNU GPL 3.0"/>
+  <property name="palette" value="skin/white"/>
+  <property name="title" value="female slash animation"/>
+  <property name="url" value="http://lpc.opengameart.org/static/lpc-style-guide/assets.html"/>
+ </properties>
+ <image source="female_slash.png" width="384" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="animation" value="sl"/>
+   <property name="direction" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="animation" value="sl"/>
+   <property name="direction" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="animation" value="sl"/>
+   <property name="direction" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="animation" value="sl"/>
+   <property name="direction" type="int" value="1"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ spritesheets/body/male/male_hurt.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="normal" tilewidth="64" tileheight="64" tilecount="6" columns="6">
+ <properties>
+  <property name="author" value="redshrike"/>
+  <property name="category" value="body"/>
+  <property name="filter_sex" type="int" value="1"/>
+  <property name="layer" type="int" value="0"/>
+  <property name="license" value="CC BY-SA 3.0;GNU GPL 3.0"/>
+  <property name="palette" value="skin/white"/>
+  <property name="title" value="male hurt animation"/>
+  <property name="url" value="http://lpc.opengameart.org/static/lpc-style-guide/assets.html"/>
+ </properties>
+ <image source="male_hurt.png" width="384" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="animation" value="hu"/>
+   <property name="direction" type="int" value="0"/>
+  </properties>
+ </tile>
+</tileset>
